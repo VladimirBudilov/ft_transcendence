02437934de4a2fc5685301348f4870fd1987b102
VladimirBudilov <109314834+VladimirBudilov@users.noreply.github.com>
2023-11-28 11:40:04 +0400

    WIP

--- Scripts/gameInitialization.js
+++ Scripts/gameInitialization.js
@@ -3,158 +3,59 @@ function OnLoadPreparation()
 
 }
 
-function createScene()
-{
-    // set the scene size
-    var WIDTH = 640,
-        HEIGHT = 360;
-
-    // set some camera attributes
+function InitCamera(WIDTH, HEIGHT) {
     var VIEW_ANGLE = 50,
         ASPECT = WIDTH / HEIGHT,
         NEAR = 0.1,
-        FAR = 10000;
-
-    var c = document.getElementById("gameCanvas");
-
-    // create a WebGL renderer, camera
-    // and a scene
-    renderer = new THREE.WebGLRenderer();
+        FAR = 1000;
     camera =
         new THREE.PerspectiveCamera(
             VIEW_ANGLE,
             ASPECT,
             NEAR,
             FAR);
-
-    scene = new THREE.Scene();
-
     // add the camera to the scene
     scene.add(camera);
-
     // set a default position for the camera
     // not doing this somehow messes up shadow rendering
-    camera.position.z = 320;
+    camera.position.z = 250;
+    camera.position.x = 0;
+    camera.position.y = -100;
+    camera.rotation.x = 30 * Math.PI / 360;
+    camera.rotation.y = 0;
+    camera.rotation.z = 0;
+}
 
-    // start the renderer
-    renderer.setSize(WIDTH, HEIGHT);
-
-    // attach the render-supplied DOM element
-    c.appendChild(renderer.domElement);
-
-    // set up the playing surface plane
-    var planeWidth = fieldWidth,
-        planeHeight = fieldHeight,
-        planeQuality = 10;
-
-    // create the paddle1's material
-    var paddle1Material =
-        new THREE.MeshLambertMaterial(
-            {
-                color: 0x1B32C0
-            });
-    // create the paddle2's material
-    var paddle2Material =
-        new THREE.MeshLambertMaterial(
-            {
-                color: 0xFF4045
-            });
-    // create the plane's material
-    var planeMaterial =
-        new THREE.MeshLambertMaterial(
-            {
-                color: 0x4BD121
-            });
-    // create the table's material
-    var tableMaterial =
-        new THREE.MeshLambertMaterial(
-            {
-                color: 0x111111
-            });
-    // create the pillar's material
-    var pillarMaterial =
-        new THREE.MeshLambertMaterial(
-            {
-                color: 0x534d0d
-            });
-    // create the ground's material
-    var groundMaterial =
-        new THREE.MeshLambertMaterial(
-            {
-                color: 0x888888
-            });
-
-
-    // create the playing surface plane
-    var plane = new THREE.Mesh(
-
-        new THREE.PlaneGeometry(
-            planeWidth * 0.95,	// 95% of table width, since we want to show where the ball goes out-of-bounds
-            planeHeight,
-            planeQuality,
-            planeQuality),
-
-        planeMaterial);
-
-    scene.add(plane);
-    plane.receiveShadow = true;
-
-    var table = new THREE.Mesh(
-
-        new THREE.CubeGeometry(
-            planeWidth * 1.05,	// this creates the feel of a billiards table, with a lining
-            planeHeight * 1.03,
-            100,				// an arbitrary depth, the camera can't see much of it anyway
-            planeQuality,
-            planeQuality,
-            1),
-
-        tableMaterial);
-    table.position.z = -51;	// we sink the table into the ground by 50 units. The extra 1 is so the plane can be seen
-    scene.add(table);
-    table.receiveShadow = true;
-
-    // // set up the sphere vars
-    // lower 'segment' and 'ring' values will increase performance
-    var radius = 5,
-        segments = 6,
-        rings = 6;
-
-    // // create the sphere's material
+function InitBall() {
+    var radius = 7,
+        segments = 100,
+        rings = 100;
     var sphereMaterial =
         new THREE.MeshLambertMaterial(
             {
                 color: 0xD43001
             });
-
-    // Create a ball with sphere geometry
     ball = new THREE.Mesh(
-
         new THREE.SphereGeometry(
             radius,
             segments,
             rings),
-
         sphereMaterial);
-
-    // // add the sphere to the scene
     scene.add(ball);
-
     ball.position.x = 0;
     ball.position.y = 0;
-    // set ball above the table surface
-    ball.position.z = radius;
+    ball.position.z = radius/2;
     ball.receiveShadow = true;
     ball.castShadow = true;
+}
 
-    // // set up the paddle vars
+function InitPaddle(paddle1Material) {
+    var paddle;
     paddleWidth = 10;
     paddleHeight = 30;
     paddleDepth = 10;
-    paddleQuality = 1;
-
-    paddle1 = new THREE.Mesh(
-
+    paddleQuality = 10;
+    paddle = new THREE.Mesh(
         new THREE.CubeGeometry(
             paddleWidth,
             paddleHeight,
@@ -162,105 +63,37 @@ function createScene()
             paddleQuality,
             paddleQuality,
             paddleQuality),
-
         paddle1Material);
 
     // // add the sphere to the scene
-    scene.add(paddle1);
-    paddle1.receiveShadow = true;
-    paddle1.castShadow = true;
+    scene.add(paddle);
+    paddle.receiveShadow = true;
+    paddle.castShadow = true;
+    return paddle;
+}
 
-    paddle2 = new THREE.Mesh(
-
-        new THREE.CubeGeometry(
-            paddleWidth,
-            paddleHeight,
-            paddleDepth,
-            paddleQuality,
-            paddleQuality,
-            paddleQuality),
-
-        paddle2Material);
-
-    // // add the sphere to the scene
-    scene.add(paddle2);
-    paddle2.receiveShadow = true;
-    paddle2.castShadow = true;
-
-    // set paddles on each side of the table
-    paddle1.position.x = -fieldWidth/2 + paddleWidth;
-    paddle2.position.x = fieldWidth/2 - paddleWidth;
-
-    // lift paddles over playing surface
-    paddle1.position.z = paddleDepth;
-    paddle2.position.z = paddleDepth;
-
-    // we iterate 10x (5x each side) to create pillars to show off shadows
-    // this is for the pillars on the left
-    for (var i = 0; i < 5; i++)
-    {
-        var backdrop = new THREE.Mesh(
-
-            new THREE.CubeGeometry(
-                30,
-                30,
-                300,
-                1,
-                1,
-                1 ),
-
-            pillarMaterial);
-
-        backdrop.position.x = -50 + i * 100;
-        backdrop.position.y = 230;
-        backdrop.position.z = -30;
-        backdrop.castShadow = true;
-        backdrop.receiveShadow = true;
-        scene.add(backdrop);
-    }
-    // we iterate 10x (5x each side) to create pillars to show off shadows
-    // this is for the pillars on the right
-    for (var i = 0; i < 5; i++)
-    {
-        var backdrop = new THREE.Mesh(
-
-            new THREE.CubeGeometry(
-                30,
-                30,
-                300,
-                1,
-                1,
-                1 ),
-
-            pillarMaterial);
-
-        backdrop.position.x = -50 + i * 100;
-        backdrop.position.y = -230;
-        backdrop.position.z = -30;
-        backdrop.castShadow = true;
-        backdrop.receiveShadow = true;
-        scene.add(backdrop);
-    }
-
-    // finally we finish by adding a ground plane
-    // to show off pretty shadows
+function InitGround() {
+    var groundMaterial =
+        new THREE.MeshLambertMaterial(
+            {
+                color: 0x888888
+            });
     var ground = new THREE.Mesh(
-
         new THREE.CubeGeometry(
             1000,
             1000,
             3,
             1,
             1,
-            1 ),
+            1),
 
         groundMaterial);
-    // set ground to arbitrary z position to best show off shadowing
     ground.position.z = -132;
     ground.receiveShadow = true;
     scene.add(ground);
+}
 
-    // // create a point light
+function InitLight() {
     pointLight =
         new THREE.PointLight(0xF8D898);
 
@@ -280,7 +113,82 @@ function createScene()
     spotLight.intensity = 1.5;
     spotLight.castShadow = true;
     scene.add(spotLight);
+}
 
-    // MAGIC SHADOW CREATOR DELUXE EDITION with Lights PackTM DLC
+function InitGameField() {
+    var planeWidth = fieldWidth,
+        planeHeight = fieldHeight,
+        planeQuality = 10;
+    var planeMaterial =
+        new THREE.MeshLambertMaterial(
+            {
+                color: 0x4BD121
+            });
+    var pillarMaterial =
+        new THREE.MeshLambertMaterial(
+            {
+                color: 0x534d0d
+            });
+    var plane = new THREE.Mesh(
+        new THREE.PlaneGeometry(
+            planeWidth * 0.95,	// 95% of table width, since we want to show where the ball goes out-of-bounds
+            planeHeight,
+            planeQuality,
+            planeQuality),
+        planeMaterial);
+    scene.add(plane);
+    plane.receiveShadow = true;
+    return {planeWidth, planeHeight, planeQuality};
+}
+
+function InitGameTable(planeWidth, planeHeight, planeQuality) {
+    var tableMaterial =
+        new THREE.MeshLambertMaterial(
+            {
+                color: 0x111111
+            });
+    var table = new THREE.Mesh(
+        new THREE.CubeGeometry(
+            planeWidth * 1.05,	// this creates the feel of a billiards table, with a lining
+            planeHeight * 1.03,
+            100,				// an arbitrary depth, the camera can't see much of it anyway
+            planeQuality,
+            planeQuality,
+            1),
+        tableMaterial);
+    table.position.z = -51;	// we sink the table into the ground by 50 units. The extra 1 is so the plane can be seen
+    scene.add(table);
+    table.receiveShadow = true;
+}
+
+function createScene()
+{
+    var gameCanvas = document.getElementById("gameCanvas");
+    // set the scene size 
+    var WIDTH = 640,
+        HEIGHT = 360;
+    renderer = new THREE.WebGLRenderer();
+    scene = new THREE.Scene();
+    InitCamera(WIDTH, HEIGHT);
+    // start the renderer
+    renderer.setSize(WIDTH, HEIGHT);
+    // attach the render-supplied DOM element
+    gameCanvas.appendChild(renderer.domElement);
+    // set up the playing surface plane
+    var {planeWidth, planeHeight, planeQuality} = InitGameField();
+    InitGameTable(planeWidth, planeHeight, planeQuality);
+    InitBall();
+    paddle1 = InitPaddle((new THREE.MeshLambertMaterial(
+        {
+            color: 0x1B32C0
+        })))
+    paddle2 = InitPaddle((new THREE.MeshLambertMaterial(
+        {
+            color: 0xFF4045
+        })))
+    paddle1.position.x = -fieldWidth/2 + paddleWidth;
+    paddle2.position.x =  fieldWidth/2 - paddleWidth;
+    InitGround();
+    InitLight();
     renderer.shadowMapEnabled = true;
 }
