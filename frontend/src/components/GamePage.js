a3a84d2173b0c8deae7a3545448c5a3b7a25f717
Victor Chizhov <victor.chizhov93@gmail.com>
2023-12-21 18:11:23 +0400

    WIP

--- frontend/src/components/GamePage.js
+++ frontend/src/components/GamePage.js
@@ -1,6 +1,10 @@
+import { lol } from './test.js';
+ 
 export default class GamePage {
     constructor() {
         document.title = 'Game Page';
+        // this.game = new Game();
+        lol();
     }
 
     async getHtml() {
@@ -9,7 +13,7 @@ export default class GamePage {
             <div class="container">
                 <div class="row">
                         <div class="col-md-9">
-                            <object type="text/html" data="../../index.html" width="100%" height="500px"></object>
+                            <iframe src="/Users/vchizhov/Desktop/ft_transcendence/game/index.html" width="100%" height="500px" frameborder="0"></iframe>
                         </div>
                         <div class="col-md-3">
                             LOL2
--- game/Scripts/tournamentLogic.js
+++ game/Scripts/tournamentLogic.js
@@ -199,3 +199,4 @@ function ReadInput() {
         tournament.StartTournament();
     }
 }
+
